124d9e23f8230f8c125dd4b20f65676266d1df07
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-03-10 12:21:26 -0700

    Create a program that reads from stdin

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/agent-e11/random_go
 
-go 1.18
+go 1.21
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+
+    for true {
+        input, err := reader.ReadString('\n')
+        if err != nil {
+            if err == io.EOF {
+                fmt.Println("End of file. Quitting...")
+            } else {
+                fmt.Printf("Error: %v", err)
+            }
+            return
+        }
+        if strings.TrimSpace(input) == "q" {
+            fmt.Println("Quitting...")
+            return
+        }
+        fmt.Printf("Your input: %s", input)
+    }
+}
+
